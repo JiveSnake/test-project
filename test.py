ceeeab16ad063a3fffdd3992a687072f52f71062
Sean Landis <sean.a.landis@gmail.com>
2023-04-16 15:26:53 -0700

    hi chaya

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
-
+# hi chaya
 X = "84014298546"  # replace with the desired string of digits
 
-weights = [3, 1, 3, 1, 3, 1, 3, 1, 3, 1, 3]  # replicate the pattern [3, 1] six times
+weights = [3, 1, 3, 1, 3, 1, 3, 1, 3, 1, 3]
 index = 0
 sum = 0
 for char in X:
@@ -11,3 +11,4 @@ checksum = (10 - sum % 10) % 10  # calculate the checksum
 result = X + str(checksum)
 print(checksum)
 print(result)
+# hi chaya
